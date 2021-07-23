202a430af150d406da8585978b37b6f5df220b44
akal-bytes <akalerupe@gmail.com>
2021-07-23 15:59:03 +0300

    school project

--- student/views.py
+++ student/views.py
@@ -1,3 +1,8 @@
+from django.forms.forms import Form
 from django.shortcuts import render
+from .forms import StudentRegistrationForm
 
-# Create your views here.
+
+def register_student(request):
+    form=StudentRegistrationForm()
+    return render(request,"register_student.html",{"form":form})
